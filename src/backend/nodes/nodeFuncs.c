b1ce16caf9fae1d70d2e6ada4a0a0830909f805a
zhangliang10 <zhangliang10@asiainfo.com>
2018-03-15 14:03:23 +0800

    修正ClusterReduce驱动死锁问题，该死锁是因为缺少驱动qual中的PARAM_EXEC类型的Param。 相关SQL为TPCH模型2的变种SQL： select         s_acctbal,         s_name,         n_name,         p_partkey,         p_mfgr,         s_address,         s_phone,         s_comment from         part,         supplier,         partsupp,         nation,         region where         p_partkey = ps_partkey         and s_suppkey = ps_suppkey         and p_size = '5'         and p_type like '%ER'         and s_nationkey = n_nationkey         and n_regionkey = r_regionkey         and r_name = 'EUROPE'         and ps_supplycost = (                 select                         min(ps_supplycost)                 from                         partsupp,                         supplier,                         nation,                         region                 where                          s_suppkey = ps_suppkey                         and s_nationkey = n_nationkey                         and n_regionkey = r_regionkey                         and r_name = 'EUROPE'         ) order by         s_acctbal desc,         n_name,         s_name,         p_partkey;

--- src/backend/nodes/nodeFuncs.c
+++ src/backend/nodes/nodeFuncs.c
@@ -25,6 +25,16 @@
 #include "utils/lsyscache.h"
 
 
+#ifdef ADB
+typedef bool (*WalkerFunc) ();
+
+typedef struct QualWalkContext {
+	WalkerFunc		walker_func;
+	void		   *walker_context;
+	ExprContext	   *econtext;
+} QualWalkContext;
+#endif
+
 static bool expression_returns_set_walker(Node *node, void *context);
 static int	leftmostLoc(int loc1, int loc2);
 static bool fix_opfuncids_walker(Node *node, void *context);
@@ -3876,13 +3886,53 @@ planstate_exec_walk_hashjoin(HashJoinState *node,
 	return false;
 }
 
+static bool
+planstate_exec_walk_param(Node *expr, void *context)
+{
+	if (IsA(expr, Param))
+	{
+		QualWalkContext *qual_walk_context = (QualWalkContext *) context;
+		Param *expression = (Param *) expr;
+		int thisParamId = expression->paramid;
+		ParamExecData  *prm = &(qual_walk_context->econtext->ecxt_param_exec_vals[thisParamId]);
+
+		if (prm->execPlan)
+		{
+			SubPlanState *node = (SubPlanState *) (prm->execPlan);
+			if (qual_walk_context->walker_func(node->planstate, qual_walk_context->walker_context))
+				return true;
+		}
+	}
+
+	return false;
+}
+
+static void
+planstate_exec_walk_qual(List *qual, QualWalkContext *context)
+{
+	ListCell	   *lc;
+
+	foreach (lc, qual)
+	{
+		ExprState  *clause = (ExprState *) lfirst(lc);
+		Expr	   *expr = clause->expr;
+
+		(void) expression_tree_walker((Node *) expr,
+									  planstate_exec_walk_param,
+									  (void *) context);
+	}
+}
+
 bool
 planstate_tree_exec_walker(PlanState *planstate,
 						   bool (*walker) (),
 						   void *context)
 {
-	Plan	   *plan = planstate->plan;
-	ListCell   *lc;
+	Plan		   *plan = planstate->plan;
+	ExprContext	   *econtext = planstate->ps_ExprContext;
+	List		   *qual = planstate->qual;
+	ListCell	   *lc;
+	QualWalkContext qual_walk_context;
 
 	switch (nodeTag(plan))
 	{
@@ -3944,6 +3994,12 @@ planstate_tree_exec_walker(PlanState *planstate,
 				return true;
 			break;
 	}
+
+	/* qual-s */
+	qual_walk_context.walker_func = walker;
+	qual_walk_context.walker_context = context;
+	qual_walk_context.econtext = econtext;
+	planstate_exec_walk_qual(qual, &qual_walk_context);
 
 	/* subPlan-s */
 	if (planstate_walk_subplans(planstate->subPlan, walker, context))
