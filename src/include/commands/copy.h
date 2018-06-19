1c03323236f2a7311294a8e6e4b77dbedf47e118
xiaoby <xiaoby@asiainfo.com>
2018-06-19 17:32:04 +0800

    重写copy命令的集群实现,支持触发器提升性能

--- src/include/commands/copy.h
+++ src/include/commands/copy.h
@@ -44,6 +44,7 @@ extern DestReceiver *CreateCopyDestReceiver(void);
 #ifdef ADB
 extern CopyState pgxcMatViewBeginCopyTo(Relation mvrel);
 extern int64 pgxcDoCopyTo(CopyState cstate);
+extern void DoClusterCopy(CopyStmt *stmt);
 #endif /* ADB */
 
 #endif							/* COPY_H */
