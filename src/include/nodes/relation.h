49cb583326dd77d21e3db7a9a28415b58b540584
xiaoby <xiaoby@asiainfo.com>
2018-07-10 17:45:30 +0800

    使用辅助表添加对or和is null的支持,更改USE_AUX_KEY为USE_AUX_CTID,GUC变量use_aux_max_times为单次查询最大使用远端辅助表的次数

--- src/include/nodes/relation.h
+++ src/include/nodes/relation.h
@@ -137,6 +137,7 @@ typedef struct PlannerGlobal
 
 #ifdef ADB
 	bool		clusterPlanOK;	/* cluster plan potentially OK? */
+	int			usedRemoteAux;	/* count of used of auxiliary table for test remote node execute on */
 #endif /* ADB */
 } PlannerGlobal;
 
