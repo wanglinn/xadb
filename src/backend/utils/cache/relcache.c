b42edf72b3f82f5e5af321b5b091bbe0368b7dbf
zhangliang10 <zhangliang10@asiainfo.com>
2018-07-16 16:16:33 +0800

    修正VACUUM FULL;没有下发执行的问题

--- src/backend/utils/cache/relcache.c
+++ src/backend/utils/cache/relcache.c
@@ -1350,7 +1350,7 @@ RelationBuildDesc(Oid targetRelId, bool insertIt)
 		relation->rd_rsdesc = NULL;
 
 #ifdef ADB
-	if (IS_PGXC_COORDINATOR &&
+	if (IsCnNode() &&
 		relation->rd_id >= FirstNormalObjectId &&
 		!IsAutoVacuumWorkerProcess())
 		RelationBuildLocator(relation);
