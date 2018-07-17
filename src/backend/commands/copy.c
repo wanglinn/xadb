b5455ac492a4a5aa0cdd530221a3a2e07206509f
xiaoby <xiaoby@asiainfo.com>
2018-07-17 11:28:47 +0800

    cluster copy增加检查远端是否有异常的代码

--- src/backend/commands/copy.c
+++ src/backend/commands/copy.c
@@ -403,6 +403,7 @@ static bool CopyGetInt16(CopyState cstate, int16 *val);
 
 #ifdef ADB
 static uint64 CoordinatorCopyFrom(CopyState cstate);
+static bool CopyFinishHook(void *context, struct pg_conn *conn, PQNHookFuncType type,  ...);
 static TupleTableSlot* NextLineCallTrigger(CopyState cstate, ExprContext *econtext, void *data);
 static TupleTableSlot* NextRowFromTuplestore(CopyState cstate, ExprContext *econtext, void *data);
 static TupleTableSlot* AddNumberNextCopyFrom(CopyState cstate, ExprContext *econtext, void *data);
@@ -5285,6 +5286,8 @@ static uint64 CoordinatorCopyFrom(CopyState cstate)
 	MemoryContext		oldcontext = CurrentMemoryContext;
 
 	ErrorContextCallback errcallback;
+	time_t last_time;
+	time_t cur_time;
 	StringInfoData	buf;
 	/* CommandId	mycid = GetCurrentCommandId(true); */
 	ExprDoneCond done;
@@ -5332,12 +5335,19 @@ static uint64 CoordinatorCopyFrom(CopyState cstate)
 	errcallback.previous = error_context_stack;
 	error_context_stack = &errcallback;
 
-	for(;;)
+	for(last_time = time(NULL);;)
 	{
 		TupleTableSlot *slot;
 
 		CHECK_FOR_INTERRUPTS();
 
+		cur_time = time(NULL);
+		if (cur_time != last_time)
+		{
+			/* check datanode error */
+			PQNListExecFinish(cstate->list_connect, NULL, CopyFinishHook, NULL, false);
+			last_time = cur_time;
+		}
 		ExecClearTuple(cstate->cs_tupleslot);
 		if (cstate->cs_tsConvert != NULL)
 			ExecClearTuple(cstate->cs_tsConvert);
@@ -5408,6 +5418,8 @@ static uint64 CoordinatorCopyFrom(CopyState cstate)
 		}
 	}
 	PQNFlush(cstate->list_connect, true);
+	PQNListExecFinish(cstate->list_connect, NULL, CopyFinishHook, NULL, true);
+
 
 	if (estate->es_result_relations)
 	{
@@ -5438,6 +5450,44 @@ static uint64 CoordinatorCopyFrom(CopyState cstate)
 	ExecResetTupleTable(estate->es_tupleTable, false);
 
 	return cstate->count_tuple;
+}
+
+static bool CopyFinishHook(void *context, struct pg_conn *conn, PQNHookFuncType type,  ...)
+{
+	va_list args;
+	switch(type)
+	{
+	case PQNHFT_ERROR:
+		ereport(ERROR, (errmsg("%m")));
+		break;
+	case PQNHFT_COPY_OUT_DATA:
+		ereport(ERROR,
+				(errcode(ERRCODE_INTERNAL_ERROR),
+				 errmsg("remote node should not copy out data")));
+		break;
+	case PQNHFT_COPY_IN_ONLY:
+		break;
+	case PQNHFT_RESULT:
+		{
+			PGresult *res;
+			va_start(args, type);
+			res = va_arg(args, PGresult*);
+			if(res)
+			{
+				ExecStatusType status = PQresultStatus(res);
+				if(status == PGRES_FATAL_ERROR)
+					PQNReportResultError(res, conn, ERROR, true);
+				else if(status == PGRES_COPY_IN)
+					PQputCopyEnd(conn, NULL);
+			}
+			va_end(args);
+		}
+		break;
+	default:
+		ereport(ERROR, (errmsg("unknown PQNHookFuncType %d for copy", type)));
+		break;
+	}
+	return false;
 }
 
 static TupleTableSlot* NextLineCallTrigger(CopyState cstate, ExprContext *econtext, void *data)
