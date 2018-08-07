5f6352e1c0f2c7227b9009ec95369989cbc963e6
zhangliang10 <zhangliang10@asiainfo.com>
2018-08-07 20:31:43 +0800

    1、RelationBuildLocator取消对autovacuum worker的限制。 2、coordinator在autovacuum时自动刷新统计信息。

--- src/backend/commands/vacuum.c
+++ src/backend/commands/vacuum.c
@@ -57,6 +57,7 @@
 #include "catalog/heap.h"
 #include "commands/defrem.h"
 #include "executor/executor.h"
+#include "intercomm/inter-comm.h"
 #include "nodes/makefuncs.h"
 #include "pgxc/execRemote.h"
 #include "pgxc/pgxc.h"
@@ -1836,6 +1837,8 @@ get_remote_relstat(char *nspname, char *relname, bool replicated,
 
 	/* Make up query string */
 	initStringInfo(&query);
+	if (IsCoordMaster() && IsAutoVacuumWorkerProcess())
+		appendStringInfo(&query, "ANALYZE %s.%s;", nspname, relname);
 	appendStringInfo(&query, "SELECT c.relpages, "
 									"c.reltuples, "
 									"c.relfrozenxid "
@@ -1843,7 +1846,7 @@ get_remote_relstat(char *nspname, char *relname, bool replicated,
 							 "ON c.relnamespace = n.oid "
 							 "WHERE n.nspname = '%s' "
 							 "AND c.relname = '%s'",
-					 nspname, relname);
+							 nspname, relname);
 
 	/* Build up RemoteQuery */
 	step = makeNode(RemoteQuery);
@@ -1960,6 +1963,25 @@ get_remote_relstat(char *nspname, char *relname, bool replicated,
 	}
 }
 
+static void
+vacuum_rel_other_coordinator(char *nspname, char *relname)
+{
+	StringInfoData		query;
+	RemoteQuery		   *step;
+
+	initStringInfo(&query);
+	appendStringInfo(&query, "ANALYZE %s.%s", nspname, relname);
+
+	step = makeNode(RemoteQuery);
+	step->combine_type = COMBINE_TYPE_NONE;
+	step->exec_nodes = NULL;
+	step->sql_statement = query.data;
+	step->force_autocommit = true;
+	step->exec_type = EXEC_ON_COORDS;
+	(void) ExecInterXactUtility(step, GetCurrentInterXactState());
+	pfree(query.data);
+	pfree(step);
+}
 
 /*
  * Coordinator does not contain any data, so we never need to vacuum relations.
@@ -2067,6 +2089,9 @@ vacuum_rel_coordinator(Relation onerel, bool is_outer)
 							min_frozenxid,
 							InvalidMultiXactId,
 							is_outer);
+
+		if (IsCoordMaster() && IsAutoVacuumWorkerProcess())
+			vacuum_rel_other_coordinator(nspname, relname);
 	}
 }
 #endif
