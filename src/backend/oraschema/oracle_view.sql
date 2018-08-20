588081c0104957eaff415dc95f7d7f2d58202ca9
zhangliang10 <zhangliang10@asiainfo.com>
2018-08-20 18:20:27 +0800

    补充oracle schema下的视图定义（由党宏博添加）

--- src/backend/oraschema/oracle_view.sql
+++ src/backend/oraschema/oracle_view.sql
@@ -14,20 +14,216 @@ CREATE RULE insert_dual AS ON INSERT TO oracle.dual DO INSTEAD NOTHING;
 CREATE RULE update_dual AS ON UPDATE TO oracle.dual DO INSTEAD NOTHING;
 CREATE RULE delete_dual AS ON DELETE TO oracle.dual DO INSTEAD NOTHING;
 
-CREATE OR REPLACE VIEW oracle.all_constraints AS
-    SELECT UPPER(con.table_schema) AS owner,
-        UPPER(con.table_name) AS table_name,
-        UPPER(con.constraint_name) AS constraint_name,
-        'P'::varchar2(10) AS constraint_type
-    FROM information_schema.constraint_table_usage con
-    JOIN pg_roles rol
-    ON 1 = 1
-    WHERE con.table_schema NOT IN ('information_schema', 'pg_catalog', 'oracle')
-    AND con.table_schema NOT LIKE 'pg_toast%'
-    AND rol.rolname = CURRENT_USER
-    ORDER BY con.table_schema, con.table_name;
+GRANT SELECT ON oracle.dual TO PUBLIC;
 
-CREATE OR REPLACE VIEW oracle.all_cons_columns AS
+CREATE OR REPLACE VIEW  oracle.dba_tables
+AS
+SELECT
+upper(nsp.nspname::text)::varchar2(30) AS owner,
+upper(cls.relname::text)::varchar2(30) AS table_name,
+cls.reltuples AS  NUM_ROWS,
+cls.relpages AS  BLOCKS
+FROM pg_class cls
+JOIN pg_roles rol ON rol.oid = cls.relowner
+JOIN pg_namespace nsp ON nsp.oid = cls.relnamespace
+where 1=1
+AND cls.relkind='r'
+AND (nsp.nspname <> ALL (ARRAY['information_schema'::name, 'pg_catalog'::name,'oracle'::name]))
+AND nsp.nspname !~~ 'pg_toast%'::text;
+
+GRANT SELECT ON oracle.dba_tables TO PUBLIC;
+
+CREATE OR REPLACE VIEW  oracle.user_tables
+AS
+SELECT *
+FROM oracle.dba_tables
+WHERE owner=upper(CURRENT_USER);
+
+GRANT SELECT ON oracle.user_tables TO PUBLIC;
+
+CREATE OR REPLACE VIEW  oracle.dba_tab_columns
+AS
+SELECT
+    upper(clm.table_schema::text) AS owner,
+    upper(clm.table_name::text) AS table_name,
+    upper(clm.column_name::text) AS column_name,
+    clm.ordinal_position AS COLUMN_ID,
+    upper(clm.data_type::text) AS data_type,
+    clm.numeric_precision AS data_precision,
+    clm.numeric_scale AS data_scale,
+    COALESCE(clm.character_maximum_length,clm.numeric_precision) AS  DATA_LENGTH,
+    CASE  clm.is_nullable
+       WHEN 'YES'::TEXT then 'Y'::TEXT
+       WHEN 'NO'::TEXT then 'N'::TEXT
+       ELSE NULL::text
+    END AS NULLABLE,
+    --COALESCE(clm.is_nullable,'Y','N') AS NULLABLE,
+    clm.column_default AS DATA_DEFAULT
+   FROM information_schema.columns clm
+     JOIN pg_roles rol ON rol.rolname = clm.table_schema::name
+  WHERE (clm.table_schema::text <> ALL (ARRAY['information_schema'::name, 'pg_catalog'::name,'oracle'::name]))
+  AND clm.table_schema::text !~~ 'pg_toast%'::text
+  ORDER BY clm.table_schema, clm.table_name,clm.ordinal_position;
+
+GRANT SELECT ON oracle.dba_tab_columns TO PUBLIC;
+
+CREATE OR REPLACE VIEW  oracle.user_tab_columns
+AS
+SELECT *
+FROM oracle.dba_tab_columns
+WHERE owner=upper(CURRENT_USER);
+
+GRANT SELECT ON oracle.user_tab_columns TO PUBLIC;
+
+CREATE OR REPLACE VIEW  oracle.dba_ind_columns
+AS
+select
+    upper(nsp.nspname) as INDEX_OWNER,
+    upper(i.relname) AS INDEX_NAME,
+    upper(t.relnamespace::regnamespace::text) AS TABLE_OWNER,
+    upper(t.relname)  AS  TABLE_NAME,
+    upper(a.attname) AS COLUMN_NAME,
+    a.attnum AS COLUMN_POSITION
+from
+    pg_class t,
+    pg_class i,
+    pg_index ix,
+    pg_attribute a,
+    pg_namespace nsp
+where
+     t.oid = ix.indrelid
+    and nsp.oid = i.relnamespace
+    and i.oid = ix.indexrelid
+    and a.attrelid = t.oid
+    and a.attnum = ANY(ix.indkey)
+    and t.relkind = 'r'
+    and i.relkind = 'i'
+    AND (nsp.nspname <> ALL (ARRAY['information_schema'::name, 'pg_catalog'::name,'oracle'::name]))
+    AND nsp.nspname !~~ 'pg_toast%'::text
+order by
+    t.relname,
+    i.relname;
+
+GRANT SELECT ON oracle.dba_ind_columns TO PUBLIC;
+
+CREATE OR REPLACE VIEW  oracle.user_ind_columns
+AS
+SELECT *
+FROM oracle.dba_ind_columns
+WHERE INDEX_OWNER=upper(CURRENT_USER);
+
+GRANT SELECT ON oracle.user_ind_columns TO PUBLIC;
+
+CREATE OR REPLACE VIEW  oracle.dba_indexes
+AS
+select
+    upper(nsp.nspname) as OWNER,
+    upper(i.relname) AS INDEX_NAME,
+    upper(am.amname) AS INDEX_TYPE,
+    upper(t.relnamespace::regnamespace::text) AS TABLE_OWNER,
+    upper(t.relname)  AS  TABLE_NAME,
+    'TABLE' AS  TABLE_TYPE,
+    CASE  ix.indisunique
+       WHEN true then 'UNIQUE'::TEXT
+       WHEN false then 'NONUNIQUE'::TEXT
+       ELSE NULL::text
+    END AS UNIQUENESS
+from
+    pg_class t,
+    pg_class i,
+    pg_index ix,
+    pg_am   am,
+    pg_namespace nsp
+where
+     t.oid = ix.indrelid
+    and nsp.oid = i.relnamespace
+    and i.oid = ix.indexrelid
+    and t.relkind = 'r'
+    and i.relkind = 'i'
+    AND (nsp.nspname <> ALL (ARRAY['information_schema'::name, 'pg_catalog'::name,'oracle'::name]))
+    AND nsp.nspname !~~ 'pg_toast%'::text
+    and am.oid=i.relam
+order by
+    t.relname,
+    i.relname;
+
+GRANT SELECT ON oracle.dba_indexes TO PUBLIC;
+
+CREATE OR REPLACE VIEW  oracle.user_indexes
+AS
+SELECT *
+FROM oracle.dba_indexes
+WHERE OWNER=upper(CURRENT_USER);
+
+GRANT SELECT ON oracle.user_indexes TO PUBLIC;
+
+CREATE OR REPLACE VIEW  oracle.dba_constraints
+AS
+SELECT upper(con.table_schema::text) AS OWNER,
+   upper(con.table_name::text) AS TABLE_NAME,
+   upper(con.constraint_name::text) AS CONSTRAINT_NAME,
+   'P'::varchar2(10) AS CONSTRAINT_TYPE,
+   upper(con.table_schema::text) AS INDEX_OWNER,
+   upper(con.constraint_name::text) AS INDEX_NAME
+  FROM information_schema.constraint_table_usage con
+    JOIN pg_roles rol ON rol.rolname = con.table_schema::name
+ WHERE (con.table_schema::text <> ALL (ARRAY['information_schema'::name, 'pg_catalog'::name,'oracle'::name]))
+ AND con.table_schema::text !~~ 'pg_toast%'::text
+ ORDER BY con.table_schema, con.table_name;
+
+GRANT SELECT ON oracle.dba_constraints TO PUBLIC;
+
+CREATE OR REPLACE VIEW  oracle.user_constraints
+AS
+SELECT *
+FROM oracle.dba_constraints
+WHERE OWNER=upper(CURRENT_USER);
+
+GRANT SELECT ON oracle.user_constraints TO PUBLIC;
+
+CREATE OR REPLACE VIEW  oracle.dba_objects
+AS
+SELECT upper(nsp.nspname::text)::varchar2(30) AS owner,
+    upper(c.relname::text)::varchar2(30) AS object_name,
+    CASE c.relkind
+        WHEN 'r' THEN 'TABLE'
+        WHEN 'v' THEN 'VIEW'
+        WHEN 'm' THEN 'MATERIALIZED VIEW'
+        WHEN 'i' THEN 'INDEX'
+        WHEN 'S' THEN 'SEQUENCE'
+        WHEN 's' THEN 'SPECIAL'
+        WHEN 'f' THEN 'FOREIGN TABLE'
+        WHEN 'p' THEN 'PARTITIONED TABLE'
+        WHEN 'c' THEN 'TYPE'
+    END as OBJECT_TYPE
+    FROM pg_class c
+     JOIN pg_roles rol ON rol.oid = c.relowner
+     JOIN pg_namespace nsp ON nsp.oid = c.relnamespace
+  WHERE (nsp.nspname <> ALL (ARRAY['information_schema'::name, 'pg_catalog'::name]))
+  AND nsp.nspname !~~ 'pg_toast%'::text
+UNION ALL
+SELECT upper(n.nspname::text)::varchar2(30) AS owner,
+   upper(p.proname) AS object_name,
+   CASE
+       WHEN p.prorettype = 'pg_catalog.trigger'::pg_catalog.regtype THEN 'TRIGGER'
+       ELSE 'FUNCTION'
+   END as OBJECT_TYPE
+FROM pg_catalog.pg_proc p
+     LEFT JOIN pg_catalog.pg_namespace n ON n.oid = p.pronamespace
+WHERE pg_catalog.pg_function_is_visible(p.oid)
+      AND(n.nspname <> ALL (ARRAY['information_schema'::name, 'pg_catalog'::name,'oracle'::name]));
+
+GRANT SELECT ON oracle.dba_objects TO PUBLIC;
+
+CREATE OR REPLACE VIEW  oracle.user_objects
+AS
+SELECT *
+FROM oracle.dba_objects
+WHERE OWNER=upper(CURRENT_USER);
+
+GRANT SELECT ON oracle.user_objects TO PUBLIC;
+
+CREATE OR REPLACE VIEW  oracle.dba_cons_columns AS
     SELECT UPPER(conclm.table_schema) AS owner,
         UPPER(conclm.table_name) AS table_name,
         UPPER(conclm.constraint_name) AS constraint_name,
@@ -37,48 +233,18 @@ CREATE OR REPLACE VIEW oracle.all_cons_columns AS
     JOIN information_schema.columns clm
     ON clm.column_name = conclm.column_name
     AND clm.table_name = conclm.table_name
-    JOIN pg_roles rol
-    ON 1 = 1
-    WHERE conclm.table_schema NOT IN ('information_schema', 'pg_catalog', 'oracle')
+    JOIN pg_roles rol ON rol.rolname = conclm.table_schema
+    WHERE conclm.table_schema NOT IN ('information_schema', 'pg_catalog','oracle')
     AND conclm.table_schema NOT LIKE 'pg_toast%'
-    AND rol.rolname = CURRENT_USER
     ORDER BY conclm.table_schema, conclm.table_name;
 
-CREATE OR REPLACE VIEW oracle.all_tab_cols AS
-    SELECT UPPER(clm.column_name) AS COLUMN_NAME,
-        UPPER(clm.data_type) AS DATA_TYPE,
-        clm.numeric_precision AS DATA_PRECISION,
-        clm.numeric_scale AS DATA_SCALE,
-        UPPER(clm.table_name) AS table_name,
-        UPPER(table_schema) AS owner
-    FROM information_schema.columns clm
-    JOIN pg_roles rol
-    ON 1 = 1
-    WHERE clm.table_schema NOT IN ('information_schema', 'pg_catalog', 'oracle')
-    AND clm.table_schema NOT LIKE 'pg_toast%'
-    AND rol.rolname = CURRENT_USER
-    ORDER BY clm.table_schema, clm.table_name;
+GRANT SELECT ON oracle.dba_cons_columns TO PUBLIC;
 
-CREATE OR REPLACE VIEW oracle.all_objects AS
-    SELECT UPPER(nsp.nspname)::varchar2(30) AS owner,
-        UPPER(cls.relname)::varchar2(30) AS object_name,
-        CASE cls.relkind
-            WHEN 'r' THEN 'TABLE'
-            WHEN 'v' THEN 'VIEW'
-        END AS object_type
-    FROM pg_class cls
-    JOIN pg_roles rol
-    ON rol.oid = cls.relowner
-    JOIN pg_namespace nsp
-    ON nsp.oid = cls.relnamespace
-    WHERE nsp.nspname NOT IN ('information_schema', 'pg_catalog', 'oracle')
-    AND nsp.nspname NOT LIKE 'pg_toast%'
-    AND rol.rolname = CURRENT_USER
-    ORDER BY nsp.nspname, cls.relname;
+CREATE OR REPLACE VIEW  oracle.user_cons_columns
+AS
+SELECT *
+FROM oracle.dba_cons_columns
+WHERE OWNER=upper(CURRENT_USER);
 
-GRANT SELECT ON oracle.dual TO PUBLIC;
-GRANT SELECT ON oracle.all_constraints TO PUBLIC;
-GRANT SELECT ON oracle.all_cons_columns TO PUBLIC;
-GRANT SELECT ON oracle.all_tab_cols TO PUBLIC;
-GRANT SELECT ON oracle.all_objects TO PUBLIC;
+GRANT SELECT ON oracle.user_cons_columns TO PUBLIC;
 
