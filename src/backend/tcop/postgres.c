2948eb41e560c9530af0641d809d8d13679bc4c5
zhaocheng <chengxiaozh@gmail.com>
2018-09-04 23:31:30 +0800

    online-expansion-reduce（支持datanode处理部分） 	之前已处理过：src/backend/executor/execCluster.c、src/backend/optimizer/path/joinpath.c、src/backend/pgxc/slot/slot.c。 	src/backend/tcop/postgres.c：增加adb_slot_enable_clean与DatanodeInClusterPlan变量

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -108,6 +108,9 @@
 #include "reduce/adb_reduce.h"
 #include "storage/procarray.h"
 #include "utils/guc.h"
+
+#include "pgxc/slot.h"
+
 #endif
 #if defined(ADB) || defined(ADB_GRAM_ORA)
 #include "nodes/nodeFuncs.h"
@@ -4593,6 +4596,10 @@ PostgresMain(int argc, char *argv[],
 
 		/* Mark transaction abort with error */
 		MarkCurrentTransactionErrorAborted();
+
+		adb_slot_enable_clean = false;
+		DatanodeInClusterPlan = false;
+
 #endif
 
 		/*
@@ -4818,6 +4825,9 @@ PostgresMain(int argc, char *argv[],
 			AcquireClusterLock(exclusive);
 			cluster_lock_held = true;
 		}
+
+		adb_slot_enable_clean = false;
+
 #endif
 
 		/*
