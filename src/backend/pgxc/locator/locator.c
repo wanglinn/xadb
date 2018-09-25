9055d79bd173420840941ed575b97f64c715f463
xiaoby <xiaoby@asiainfo.com>
2018-09-25 14:44:32 +0800

    修正datanode使用复制表或轮询表时可能导致除零错误的问题

--- src/backend/pgxc/locator/locator.c
+++ src/backend/pgxc/locator/locator.c
@@ -885,8 +885,9 @@ RelationIdBuildLocator(Oid relid)
 	 * use next time. In addition, if it is replicated,
 	 * we choose a node to use for balancing reads.
 	 */
-	if (relationLocInfo->locatorType == LOCATOR_TYPE_RROBIN ||
-		relationLocInfo->locatorType == LOCATOR_TYPE_REPLICATED)
+	if (IsCnNode() &&
+		(relationLocInfo->locatorType == LOCATOR_TYPE_RROBIN ||
+		 relationLocInfo->locatorType == LOCATOR_TYPE_REPLICATED))
 	{
 		int offset;
 		/*
