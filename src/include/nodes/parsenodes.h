c69a7ae4c6e414add9ab31e14ddd00d71e343e7f
xiaoby <xiaoby@asiainfo.com>
2018-10-15 15:01:36 +0800

    提交1e12bdddef2059ecb0dfa3cf8e35c79a8e94fca5遗忘的代码

--- src/include/nodes/parsenodes.h
+++ src/include/nodes/parsenodes.h
@@ -3489,7 +3489,7 @@ typedef struct DropNodeStmt
 } DropNodeStmt;
 
 
-
+#ifdef ADB
 typedef struct CreateSlotStmt
 {
 	NodeTag		type;
@@ -3534,7 +3534,7 @@ typedef struct CleanSlotStmt
 	char		*schema_name;
 	char		*table_name;
 } CleanSlotStmt;
-
+#endif /* ADB */
 
 
 /*
