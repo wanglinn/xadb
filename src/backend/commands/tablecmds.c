9f6d8d8784fc1825a7fa53119138bf4089dd7b21
xiaoby <xiaoby@asiainfo.com>
2018-12-29 14:37:32 +0800

    删除DefineRelation函数中没有必要再次调用RelationCacheInvalidateEntry的代码,因CommandCounterIncrement函数中已经间接调用了

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -883,8 +883,6 @@ DefineRelation(CreateStmt *stmt, char relkind, Oid ownerId,
 								inheritOids,
 								descriptor);
 		CommandCounterIncrement();
-		/* Make sure locator info gets rebuilt */
-		RelationCacheInvalidateEntry(relationId);
 	}
 
 	/* Add for pg_aux_class */
