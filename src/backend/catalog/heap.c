563982fd8068f8dd5e9cc309ada805de6784e1ba
wanglin3 <wanglin3@asiainfo.com>
2019-01-29 10:11:48 +0800

    不支持对hashmap表 ADD/DELETE/TO node操作

--- src/backend/catalog/heap.c
+++ src/backend/catalog/heap.c
@@ -1419,6 +1419,10 @@ AddRelationDistribution(Oid relid,
 		nodeoids = NULL;
 	} else
 	{
+		if (distributeby && distributeby->disttype == DISTTYPE_HASHMAP && subcluster)
+			ereport(ERROR,
+				(errcode(ERRCODE_SYNTAX_ERROR),
+					errmsg("not support CREATE hashmap table TO node")));
 		nodeoids = GetRelationDistributionNodes(subcluster, &numnodes);
 	}
 
