fd2b27e749963d4d51ab2d4b1ffb09859478b76d
xiaoby <xiaoby@asiainfo.com>
2019-03-13 20:48:35 +0800

    修正ClusterReduce的outer plan如果是Unique的时候会导致不能去重的问题

--- src/backend/executor/nodeClusterReduce.c
+++ src/backend/executor/nodeClusterReduce.c
@@ -326,8 +326,6 @@ GetSlotFromOuter(ClusterReduceState *node)
 
 			if (outerValid)
 				return outerslot;
-
-			ExecClearTuple(outerslot);
 		} else
 		{
 			/* Here we send eof to remote plan nodes */
