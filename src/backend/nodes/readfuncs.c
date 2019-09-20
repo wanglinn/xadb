d0cbde9a127bda2b6fea105d69f9eb54144acb0e
xiaoby <xiaoby@asiainfo.com>
2019-09-20 17:09:16 +0800

    out/read TargetEntry对as_location做为int处理,而不是location,READ_LOCATION_FIELD会丢弃location信息

--- src/backend/nodes/readfuncs.c
+++ src/backend/nodes/readfuncs.c
@@ -1272,7 +1272,7 @@ _readTargetEntry(void)
 	READ_INT_FIELD(resorigcol);
 	READ_BOOL_FIELD(resjunk);
 #ifdef ADB_GRAM_ORA
-	READ_LOCATION_FIELD(as_location);
+	READ_INT_FIELD(as_location);
 #endif /* ADB_GRAM_ORA */
 
 	READ_DONE();
