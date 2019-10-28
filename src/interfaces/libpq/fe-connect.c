4161f1a136422ca148414d95ff4513472e73bba4
liupc <liupc@asiainfo.com>
2019-10-28 11:20:57 +0800

    修复merge11.5后，snapsender和gxidsender与postmaster共用端口的问题。

--- src/interfaces/libpq/fe-connect.c
+++ src/interfaces/libpq/fe-connect.c
@@ -1780,14 +1780,6 @@ connectDBStart(PGconn *conn)
 	 * a bit of a cheat, but PQconnectPoll will advance it to 0 before
 	 * anything else looks at it.)
 	 */
-#ifdef ADB
-	if (conn->connect_type && strcmp(conn->connect_type, "gxidrcv") == 0)
-		conn->pversion = GXID_SEND_SOCKET;
-	else if (conn->connect_type && strcmp(conn->connect_type, "snaprcv") == 0)
-		conn->pversion = SNAP_SEND_SOCKET;
-	else
-#endif
-	conn->pversion = PG_PROTOCOL(3, 0);
 #warning "version set droped from here, change code please. see f6f735f78d9e05b757c96b036dfd31b1f9f77312"
 	conn->whichhost = -1;
 	conn->try_next_addr = false;
@@ -2210,7 +2202,15 @@ keep_going:						/* We will come back to here until there is
 		 * must persist across individual connection attempts, but we must
 		 * reset them when we start to consider a new server.
 		 */
+#ifdef ADB
+		if (conn->connect_type && strcmp(conn->connect_type, "gxidrcv") == 0)
+			conn->pversion = GXID_SEND_SOCKET;
+		else if (conn->connect_type && strcmp(conn->connect_type, "snaprcv") == 0)
+			conn->pversion = SNAP_SEND_SOCKET;
+		else
+#endif
 		conn->pversion = PG_PROTOCOL(3, 0);
+
 		conn->send_appname = true;
 #ifdef USE_SSL
 		/* initialize these values based on SSL mode */
