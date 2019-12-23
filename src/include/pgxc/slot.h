ef9647349978844b1249c1df55c90fca612e1478
xiaoby <xiaoby@asiainfo.com>
2019-12-23 18:02:40 +0800

    修改用于计算hashmap分片节点的函数nodeid_from_hashvalue为nodeoid_from_slotindex

--- src/include/pgxc/slot.h
+++ src/include/pgxc/slot.h
@@ -34,6 +34,7 @@ extern Size SlotShmemSize(void);
 extern void SlotGetInfo(int slotid, int* pnodeindex, int* pstatus);
 extern List *GetSlotNodeOids(void);
 extern bool IsSlotNodeOidsEqualOidList(const List *list);
+extern Expr* CreateNodeOidFromSlotIndexExpr(Expr *index);
 
 extern void SlotAlter(AlterSlotStmt *stmt);
 extern void SlotCreate(CreateSlotStmt *stmt);
