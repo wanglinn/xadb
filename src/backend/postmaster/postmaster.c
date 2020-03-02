dfb5d4e231bbd7d280d4c478a54e8b735fd77cb4
liupc <liupc@asiainfo.com>
2020-03-02 13:50:51 +0800

    Revert "修复重启进程后InitAuxiliaryProcess的ERROR。"
    This reverts commit cd48365ed910f3c1a2b5c951bce25c46eed01ec5.

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -3710,8 +3710,6 @@ reaper(SIGNAL_ARGS)
 		if (IsGTMNode() && pmState == PM_RUN && pid == SnapSenderPID)
 		{
 			SnapSenderPID = 0;
-			ProcGlobal->snapshotProc->pid = 0;
-			InitSharedLatch(&ProcGlobal->snapshotProc->procLatch);
 			if (EXIT_STATUS_0(exitstatus) ||
 				EXIT_STATUS_1(exitstatus))
 			{
@@ -3729,8 +3727,6 @@ reaper(SIGNAL_ARGS)
 		if (IsGTMNode() && pmState == PM_RUN && pid == GxidSenderPID)
 		{
 			GxidSenderPID = 0;
-			ProcGlobal->gxidProc->pid = 0;
-			InitSharedLatch(&ProcGlobal->gxidProc->procLatch);
 			if (EXIT_STATUS_0(exitstatus) ||
 				EXIT_STATUS_1(exitstatus))
 			{
@@ -3748,14 +3744,11 @@ reaper(SIGNAL_ARGS)
 		if (!IsGTMNode() && pid == SnapReceiverPID)
 		{
 			SnapReceiverPID = 0;
-			ProcGlobal->snapshotProc->pid = 0;
-			InitSharedLatch(&ProcGlobal->snapshotProc->procLatch);
 			if (EXIT_STATUS_0(exitstatus) ||
 				EXIT_STATUS_1(exitstatus))
 			{
 				LogChildExit(LOG,
 							 pgstat_get_backend_desc(B_ADB_SNAP_RECEIVER), pid, exitstatus);
-
 				SnapReceiverPID = StartSnapReceiver();
 			}else
 			{
@@ -3768,8 +3761,6 @@ reaper(SIGNAL_ARGS)
 		if (!IsGTMNode() && pid == GxidReceiverPID)
 		{
 			GxidReceiverPID = 0;
-			ProcGlobal->gxidProc->pid = 0;
-			InitSharedLatch(&ProcGlobal->gxidProc->procLatch);
 			if (EXIT_STATUS_0(exitstatus) ||
 				EXIT_STATUS_1(exitstatus))
 			{
