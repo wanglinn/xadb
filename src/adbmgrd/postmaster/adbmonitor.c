60e8b57846a68f042866831b4c079800aa775769
yusf3 <yusf3@asiainfo.com>
2020-04-26 18:06:38 +0800

    解决adb monitor launcher process CPU利用率100%的bug

--- src/adbmgrd/postmaster/adbmonitor.c
+++ src/adbmgrd/postmaster/adbmonitor.c
@@ -184,6 +184,7 @@ static bool get_latest_job_time(TimestampTz *tzstamp);
 static void print_work_job(void);
 static void print_workers(void);
 static void adbmonitor_exec_job(Oid jobid);
+static TimestampTz get_monitor_job_next_time(HeapTuple tup, TupleDesc tupdesc);
 
 /*
  * Main entry point for adb monitor launcher process, to be called from the
@@ -590,7 +591,6 @@ launcher_obtain_amljob(void)
 	Relation			rel_node;
 	HeapScanDesc		rel_scan;
 	HeapTuple 			tuple;
-	Form_monitor_job	monitor_job;
 	TimestampTz			timetz;
 	TimestampTz			current_time;
 	TimestampTz			timetzMin = 0;
@@ -615,15 +615,12 @@ launcher_obtain_amljob(void)
 	rel_scan = heap_beginscan_catalog(rel_node, 2, entry);
 	while ((tuple = heap_getnext(rel_scan, ForwardScanDirection)) != NULL)
 	{
-		monitor_job = (Form_monitor_job) GETSTRUCT(tuple);
-		Assert(monitor_job);
-
 		/* The job is running? */
 		if (monitor_job_running(HeapTupleGetOid(tuple)))
 			continue;
 
 		/* Find the latest monitor job */
-		timetz = monitor_job->next_time;
+		timetz = get_monitor_job_next_time(tuple, RelationGetDescr(rel_node));
 		if (!timetzMin)
 		{
 			timetzMin = timetz;
@@ -882,7 +879,7 @@ get_latest_job_time(TimestampTz *tzstamp)
 	Relation			rel_node;
 	HeapScanDesc		rel_scan;
 	HeapTuple			tuple;
-	Form_monitor_job	monitor_job;
+	TimestampTz			next_time;
 	TimestampTz			latest_jobtime = 0;
 	ScanKeyData			entry[1];
 
@@ -898,18 +895,16 @@ get_latest_job_time(TimestampTz *tzstamp)
 
 	while((tuple = heap_getnext(rel_scan, ForwardScanDirection)) != NULL)
 	{
-		monitor_job = (Form_monitor_job) GETSTRUCT(tuple);
-		Assert(monitor_job);
-
 		/* Ignore the running job  */
 		if (monitor_job_running(HeapTupleGetOid(tuple)))
 			continue;
 
+		next_time = get_monitor_job_next_time(tuple, RelationGetDescr(rel_node));
 		/* Get the latest work time */
 		if (latest_jobtime == 0)
-			latest_jobtime = monitor_job->next_time;
-		else if(latest_jobtime > monitor_job->next_time)
-			latest_jobtime = monitor_job->next_time;
+			latest_jobtime = next_time;
+		else if(latest_jobtime > next_time)
+			latest_jobtime = next_time;
 	}
 
 	heap_endscan(rel_scan);
@@ -1456,4 +1451,21 @@ print_workers(void)
 		LWLockRelease(AdbmonitorLock);
 	}
 #endif
+}
+
+static TimestampTz get_monitor_job_next_time(HeapTuple tup, TupleDesc tupdesc)
+{
+	Datum				datum;
+	bool				datumIsNull;
+
+	datum = fastgetattr(tup,
+						Anum_monitor_job_next_time,
+						tupdesc,
+						&datumIsNull);
+	if(datumIsNull)
+		ereport(ERROR, 
+				(errcode(ERRCODE_DATA_EXCEPTION),
+				(errmsg("The attribute %s can not be null",
+						NameStr(TupleDescAttr(tupdesc, Anum_monitor_job_next_time - 1)->attname)))));
+	return DatumGetTimestampTz(datum);
 }
