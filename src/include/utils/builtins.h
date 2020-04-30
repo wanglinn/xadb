b5c2286d2f19b0b83966a452fc2edee9b6bc177d
xiaoby <xiaoby@asiainfo.com>
2020-04-30 16:47:51 +0800

    删除builtins.h中不再需要的函数声明

--- src/include/utils/builtins.h
+++ src/include/utils/builtins.h
@@ -80,29 +80,6 @@ extern int	oid_cmp(const void *p1, const void *p2);
 /* regexp.c */
 extern char *regexp_fixed_prefix(text *text_re, bool case_insensitive,
 					Oid collation, bool *exact);
-#if defined(ADB_GRAM_ORA)
-extern Datum ora_regexp_count2(PG_FUNCTION_ARGS);
-extern Datum ora_regexp_count3(PG_FUNCTION_ARGS);
-extern Datum ora_regexp_count(PG_FUNCTION_ARGS);
-extern Datum ora_regexp_replace2(PG_FUNCTION_ARGS);
-extern Datum ora_regexp_replace3(PG_FUNCTION_ARGS);
-extern Datum ora_regexp_replace4(PG_FUNCTION_ARGS);
-extern Datum ora_regexp_replace5(PG_FUNCTION_ARGS);
-extern Datum ora_regexp_replace(PG_FUNCTION_ARGS);
-extern Datum ora_regexp_substr2(PG_FUNCTION_ARGS);
-extern Datum ora_regexp_substr3(PG_FUNCTION_ARGS);
-extern Datum ora_regexp_substr4(PG_FUNCTION_ARGS);
-extern Datum ora_regexp_substr5(PG_FUNCTION_ARGS);
-extern Datum ora_regexp_substr(PG_FUNCTION_ARGS);
-extern Datum ora_regexp_instr2(PG_FUNCTION_ARGS);
-extern Datum ora_regexp_instr3(PG_FUNCTION_ARGS);
-extern Datum ora_regexp_instr4(PG_FUNCTION_ARGS);
-extern Datum ora_regexp_instr5(PG_FUNCTION_ARGS);
-extern Datum ora_regexp_instr6(PG_FUNCTION_ARGS);
-extern Datum ora_regexp_instr(PG_FUNCTION_ARGS);
-extern Datum ora_regexp_like2(PG_FUNCTION_ARGS);
-extern Datum ora_regexp_like(PG_FUNCTION_ARGS);
-#endif
 
 /* ruleutils.c */
 extern bool quote_all_identifiers;
@@ -160,23 +137,6 @@ extern Datum orastr_translate(PG_FUNCTION_ARGS);
 extern Datum orastr_nls_charset_id(PG_FUNCTION_ARGS);
 extern Datum orastr_nls_charset_name(PG_FUNCTION_ARGS);
 
-/* oradate.c */
-extern Datum ora_next_day(PG_FUNCTION_ARGS);
-extern Datum last_day(PG_FUNCTION_ARGS);
-extern Datum months_between(PG_FUNCTION_ARGS);
-extern Datum add_months(PG_FUNCTION_ARGS);
-extern Datum ora_date_trunc(PG_FUNCTION_ARGS);
-extern Datum ora_timestamptz_trunc(PG_FUNCTION_ARGS);
-extern Datum ora_date_round(PG_FUNCTION_ARGS);
-extern Datum ora_timestamptz_round(PG_FUNCTION_ARGS);
-extern Datum ora_sys_now(PG_FUNCTION_ARGS);
-extern Datum ora_dbtimezone(PG_FUNCTION_ARGS);
-extern Datum ora_session_timezone(PG_FUNCTION_ARGS);
-extern Datum ora_numtoyminterval(PG_FUNCTION_ARGS);
-extern Datum ora_numtodsinterval(PG_FUNCTION_ARGS);
-extern Datum ora_to_yminterval(PG_FUNCTION_ARGS);
-extern Datum ora_to_dsinterval(PG_FUNCTION_ARGS);
-
 /* others.c */
 extern Datum ora_lnnvl(PG_FUNCTION_ARGS);
 extern Datum ora_concat(PG_FUNCTION_ARGS);
@@ -185,73 +145,6 @@ extern Datum ora_nvl2(PG_FUNCTION_ARGS);
 extern Datum ora_set_nls_sort(PG_FUNCTION_ARGS);
 extern Datum ora_nlssort(PG_FUNCTION_ARGS);
 extern Datum ora_dump(PG_FUNCTION_ARGS);
-
-/* convert.c */
-extern Datum int4_tochar(PG_FUNCTION_ARGS);
-extern Datum int8_tochar(PG_FUNCTION_ARGS);
-extern Datum float4_tochar(PG_FUNCTION_ARGS);
-extern Datum float8_tochar(PG_FUNCTION_ARGS);
-extern Datum numeric_tochar(PG_FUNCTION_ARGS);
-extern Datum text_tochar(PG_FUNCTION_ARGS);
-extern Datum timestamp_tochar(PG_FUNCTION_ARGS);
-extern Datum timestamptz_tochar(PG_FUNCTION_ARGS);
-extern Datum interval_tochar(PG_FUNCTION_ARGS);
-
-extern Datum trunc_text_toint2(PG_FUNCTION_ARGS);
-extern Datum trunc_text_toint4(PG_FUNCTION_ARGS);
-extern Datum trunc_text_toint8(PG_FUNCTION_ARGS);
-extern Datum text_toint2(PG_FUNCTION_ARGS);
-extern Datum text_toint4(PG_FUNCTION_ARGS);
-extern Datum text_toint8(PG_FUNCTION_ARGS);
-extern Datum text_tofloat4(PG_FUNCTION_ARGS);
-extern Datum text_tofloat8(PG_FUNCTION_ARGS);
-extern Datum text_todate(PG_FUNCTION_ARGS);
-extern Datum text_totimestamp(PG_FUNCTION_ARGS);
-extern Datum text_totimestamptz(PG_FUNCTION_ARGS);
-extern Datum text_tocstring(PG_FUNCTION_ARGS);
-extern Datum text_tonumber(PG_FUNCTION_ARGS);
-extern Datum float4_tonumber(PG_FUNCTION_ARGS);
-extern Datum float8_tonumber(PG_FUNCTION_ARGS);
-
-extern Datum ora_to_multi_byte(PG_FUNCTION_ARGS);
-extern Datum ora_to_single_byte(PG_FUNCTION_ARGS);
-
-/* varchar2.c */
-extern Datum varchar2in(PG_FUNCTION_ARGS);
-extern Datum varchar2out(PG_FUNCTION_ARGS);
-extern Datum varchar2recv(PG_FUNCTION_ARGS);
-extern Datum varchar2(PG_FUNCTION_ARGS);
-
-/* nvarchar2.c */
-extern Datum nvarchar2in(PG_FUNCTION_ARGS);
-extern Datum nvarchar2out(PG_FUNCTION_ARGS);
-extern Datum nvarchar2recv(PG_FUNCTION_ARGS);
-extern Datum nvarchar2(PG_FUNCTION_ARGS);
-
-/* random.c */
-extern Datum dbms_random_initialize(PG_FUNCTION_ARGS);
-extern Datum dbms_random_normal(PG_FUNCTION_ARGS);
-extern Datum dbms_random_random(PG_FUNCTION_ARGS);
-extern Datum dbms_random_seed_int(PG_FUNCTION_ARGS);
-extern Datum dbms_random_seed_varchar(PG_FUNCTION_ARGS);
-extern Datum dbms_random_string(PG_FUNCTION_ARGS);
-extern Datum dbms_random_terminate(PG_FUNCTION_ARGS);
-extern Datum dbms_random_value(PG_FUNCTION_ARGS);
-extern Datum dbms_random_value_range(PG_FUNCTION_ARGS);
-
-/* rowid.c */
-extern Datum rowid_in(PG_FUNCTION_ARGS);
-extern Datum rowid_out(PG_FUNCTION_ARGS);
-extern Datum rowid_recv(PG_FUNCTION_ARGS);
-extern Datum rowid_send(PG_FUNCTION_ARGS);
-extern Datum rowid_eq(PG_FUNCTION_ARGS);
-extern Datum rowid_ne(PG_FUNCTION_ARGS);
-extern Datum rowid_lt(PG_FUNCTION_ARGS);
-extern Datum rowid_le(PG_FUNCTION_ARGS);
-extern Datum rowid_gt(PG_FUNCTION_ARGS);
-extern Datum rowid_ge(PG_FUNCTION_ARGS);
-extern Datum rowid_larger(PG_FUNCTION_ARGS);
-extern Datum rowid_smaller(PG_FUNCTION_ARGS);
 #endif
 
 /* inet_cidr_ntop.c */
@@ -287,35 +180,5 @@ extern int32 type_maximum_size(Oid type_oid, int32 typemod);
 
 /* quote.c */
 extern char *quote_literal_cstr(const char *rawstr);
-
-#ifdef ADB
-/* backend/pgxc/pool/poolutils.c */
-extern Datum pgxc_pool_reload(PG_FUNCTION_ARGS);
-
-extern Datum pgxc_is_committed(PG_FUNCTION_ARGS);
-
-/* src/backend/pgxc/pool/pgxcnode.c */
-extern Datum pgxc_node_str (PG_FUNCTION_ARGS);
-extern Datum adb_node_oid(PG_FUNCTION_ARGS);
-
-/* src/backend/utils/adt/lockfuncs.c */
-extern Datum pgxc_lock_for_backup (PG_FUNCTION_ARGS);
-
-/* src/backend/access/rxact/rxact_comm.c */
-extern Datum rxact_wait_gid(PG_FUNCTION_ARGS);
-extern Datum rxact_get_running(PG_FUNCTION_ARGS);
-
-/* src/backend/access/transam/varsup.c */
-extern Datum current_xid(PG_FUNCTION_ARGS);
-#endif   /* ADB */
-
-#if defined(ADB) || defined(ADB_MULTI_GRAM)
-extern Datum pg_xact_status(PG_FUNCTION_ARGS);
-#endif
-
-#if defined(ADB) || defined(ADB_MULTI_GRAM)
-/* src/backend/catalog/heap.c */
-extern Datum pg_explain_infomask(PG_FUNCTION_ARGS);
-#endif
 
 #endif							/* BUILTINS_H */
