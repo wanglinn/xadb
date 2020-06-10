9436b31032a08031eb81442c40ca9f5746d2c7d8
liupc <liupc@asiainfo.com>
2020-06-10 10:27:41 +0800

    改回PG原生fsm_search_avail，添加单独函数fsm_search_avail_ext。

--- src/include/storage/fsm_internals.h
+++ src/include/storage/fsm_internals.h
@@ -60,9 +60,13 @@ typedef FSMPageData *FSMPage;
  */
 #define SlotsPerFSMPage LeafNodesPerPage
 
+#ifdef ADB
+extern int fsm_search_avail_ext(Buffer buf, uint8 minvalue, bool advancenext,
+								bool exclusive_lock_held, bool skip_flag);
+#endif
 /* Prototypes for functions in fsmpage.c */
 extern int	fsm_search_avail(Buffer buf, uint8 min_cat, bool advancenext,
-							 bool exclusive_lock_held ADB_ONLY_COMMA_ARG(bool skip_flag));
+							 bool exclusive_lock_held);
 extern uint8 fsm_get_avail(Page page, int slot);
 extern uint8 fsm_get_max_avail(Page page);
 extern bool fsm_set_avail(Page page, int slot, uint8 value);
