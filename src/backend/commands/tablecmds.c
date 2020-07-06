da66b80b0e5447b388b590d3f715f82f81ce9ac1
liupc <liupc@asiainfo.com>
2020-07-06 17:56:45 +0800

    修复ALTER TABLE ATTACH PARTITION 外表，比较子表和外表分片方式不对，报错分片方式不同的错误。

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -16576,7 +16576,8 @@ ATExecAttachPartition(List **wqueue, Relation rel, PartitionCmd *cmd)
 				 errdetail("ROW triggers with transition tables are not supported on partitions")));
 
 #ifdef ADB
-	checkTwoTblDistributebyType(attachrel, rel, true, false);
+	if (attachrel->rd_rel->relkind != RELKIND_FOREIGN_TABLE)
+		checkTwoTblDistributebyType(attachrel, rel, true, false);
 #endif
 
 	/*
