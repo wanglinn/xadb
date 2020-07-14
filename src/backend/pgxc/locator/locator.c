e1c82d964d1d2c653f7ca231c36f339c605cf5ad
gedh <gedh@asiainfo.com>
2020-07-14 15:49:13 +0800

    修复扩容后因使用hash值获取对应节点oid导致获取节点oid重复,从而导致poolmgr出现double get node connect问题.

--- src/backend/pgxc/locator/locator.c
+++ src/backend/pgxc/locator/locator.c
@@ -842,7 +842,7 @@ RelationIdBuildLocator(Oid relid)
 			++j;
 		}
 		for (j=0; j<count; ++j)
-			relationLocInfo->nodeids = lappend_oid(relationLocInfo->nodeids, oids[j]);
+			relationLocInfo->nodeids = list_append_unique_oid(relationLocInfo->nodeids, oids[j]);
 		pfree(str);
 		pfree(oids);
 		list_free_deep(list);
@@ -850,7 +850,7 @@ RelationIdBuildLocator(Oid relid)
 	else if (pgxc_class->pclocatortype != LOCATOR_TYPE_HASHMAP)
 	{
 		for (j = 0; j < pgxc_class->nodeoids.dim1; j++)
-			relationLocInfo->nodeids = lappend_oid(relationLocInfo->nodeids,
+			relationLocInfo->nodeids = list_append_unique_oid(relationLocInfo->nodeids,
 							pgxc_class->nodeoids.values[j]);
 	}
 	else
