13b3fd3a5020ba168489a84fbaee88ed6fdc719f
xiaoby <xiaoby@asiainfo.com>
2020-07-23 14:22:00 +0800

    regress跳过oracle namespace下函数的descriptions检查,也许某天我们应该检查

--- src/test/regress/sql/opr_sanity.sql
+++ src/test/regress/sql/opr_sanity.sql
@@ -365,7 +365,8 @@ WHERE p2.oid = p1.prosupport AND
 SELECT p1.oid, p1.proname
 FROM pg_proc as p1 LEFT JOIN pg_description as d
      ON p1.tableoid = d.classoid and p1.oid = d.objoid and d.objsubid = 0
-WHERE d.classoid IS NULL AND p1.oid <= 9999;
+WHERE d.classoid IS NULL AND p1.oid <= 9999
+  and pronamespace not in (select oid from pg_namespace where nspname = 'oracle');
 
 -- List of built-in leakproof functions
 --
