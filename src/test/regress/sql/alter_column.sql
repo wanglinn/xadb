d1ff4951c1122b155bd127a5db11d1b257e52355
xiaoby <xiaoby@asiainfo.com>
2020-07-30 15:04:14 +0800

    oracle兼容regress改为并行测试

--- src/test/regress/sql/alter_column.sql
+++ src/test/regress/sql/alter_column.sql
@@ -1,30 +1,30 @@
 set grammar to oracle;
 --增加列，3种语法
-create table tt(tid int,name varchar(2));
-insert into tt values(1, 'a');
-alter table tt add(dct1 varchar(10));
-alter table tt add dct2 varchar(10);
-alter table tt add column dct3 varchar(10);
-drop table tt;
+create table altercoltbl(tid int,name varchar(2));
+insert into altercoltbl values(1, 'a');
+alter table altercoltbl add(dct1 varchar(10));
+alter table altercoltbl add dct2 varchar(10);
+alter table altercoltbl add column dct3 varchar(10);
+drop table altercoltbl;
 --删除单列
-create table tt(tid int,name varchar(2));
-insert into tt values(1,'a');
-insert into tt values(2,'b');
-alter table tt drop column name;
-drop table tt;
+create table altercoltbl(tid int,name varchar(2));
+insert into altercoltbl values(1,'a');
+insert into altercoltbl values(2,'b');
+alter table altercoltbl drop column name;
+drop table altercoltbl;
 --删除多列
-create table tt(tid int,name varchar(2),tel char(10));
-alter table tt drop(name,tel);
-drop table tt;
+create table altercoltbl(tid int,name varchar(2),tel char(10));
+alter table altercoltbl drop(name,tel);
+drop table altercoltbl;
 --修改列
-create table tt(tid int,name varchar(2));
-alter table tt modify name char(10);
-alter table tt modify name default('a');
-drop table tt;
-create table tt(tid int,name varchar(2),tel char(11));
-alter table tt modify(name char(10),tel char(12));
-drop table tt;
+create table altercoltbl(tid int,name varchar(2));
+alter table altercoltbl modify name char(10);
+alter table altercoltbl modify name default('a');
+drop table altercoltbl;
+create table altercoltbl(tid int,name varchar(2),tel char(11));
+alter table altercoltbl modify(name char(10),tel char(12));
+drop table altercoltbl;
 --重命名列
-create table tt(tid int,name varchar(2));
-alter table tt rename column name to na;
-drop table tt;
+create table altercoltbl(tid int,name varchar(2));
+alter table altercoltbl rename column name to na;
+drop table altercoltbl;
