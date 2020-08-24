b98915b120cfd06ea1bdd79a2d1b17817e78a8f3
liupc <liupc@asiainfo.com>
2020-08-24 11:41:05 +0800

    dn上autovacuum 取消触发analyze。因为其他cn会触发cluster analyze并搜集统计信息。

--- src/backend/postmaster/autovacuum.c
+++ src/backend/postmaster/autovacuum.c
@@ -107,6 +107,7 @@
 #include "utils/tqual.h"
 #ifdef ADB
 #include "replication/snapreceiver.h"
+#include "pgxc/pgxc.h"
 #endif /* ADB */
 
 
@@ -3095,6 +3096,11 @@ relation_needs_vacanalyze(Oid relid,
 
 		/* Determine if this table needs vacuum or analyze. */
 		*dovacuum = force_vacuum || (vactuples > vacthresh);
+#ifdef ADB
+		if (IsDnNode())
+			*doanalyze = false;
+		else
+#endif
 		*doanalyze = (anltuples > anlthresh);
 	}
 	else
