409d1118499962dc41b8a49d309f7920b1fd6818
xiaoby <xiaoby@asiainfo.com>
2020-09-11 16:41:41 +0800

    修改adb_views中视图的schema为pg_catalog,并去除rxact_get_running视图中不再出现的AGTM名字

--- src/backend/catalog/adb_views.sql
+++ src/backend/catalog/adb_views.sql
@@ -1,7 +1,7 @@
 /*
  * PGXC system view to look for prepared transaction GID list in a cluster
  */
-CREATE OR REPLACE FUNCTION pgxc_prepared_xact()
+CREATE OR REPLACE FUNCTION pg_catalog.pgxc_prepared_xact()
 RETURNS setof text
 AS $$
 DECLARE
@@ -12,7 +12,7 @@ DECLARE
 	query_str_nodes text;
 	BEGIN
 		--Get all the node names
-		query_str_nodes := 'SELECT node_name FROM pgxc_node WHERE node_type = ''D''';
+		query_str_nodes := 'SELECT node_name FROM pgxc_node WHERE node_type = ''D'' AND node_master_oid=0';
 		FOR row_name IN EXECUTE(query_str_nodes) LOOP
 			query_str := 'EXECUTE DIRECT ON ("' || row_name.node_name || '") ''SELECT gid FROM pg_prepared_xact()''';
 			FOR row_data IN EXECUTE(query_str) LOOP
@@ -23,11 +23,11 @@ DECLARE
 	END; $$
 LANGUAGE 'plpgsql';
 
-CREATE VIEW pgxc_prepared_xacts AS
-    SELECT DISTINCT * from pgxc_prepared_xact();
+CREATE VIEW pg_catalog.pgxc_prepared_xacts AS
+    SELECT DISTINCT * from pg_catalog.pgxc_prepared_xact();
 
-CREATE OR REPLACE VIEW rxact_get_running AS
-    SELECT r.gid,r.database,CASE WHEN n.node_name IS NULL THEN 'AGTM' ELSE n.node_name END,r.backend,r.type,r.status[r.i]
+CREATE OR REPLACE VIEW pg_catalog.rxact_get_running AS
+    SELECT r.gid,r.database,n.node_name,r.backend,r.type,r.status[r.i]
       FROM (SELECT *,generate_series(1,array_length(nodes,1)) AS i
         FROM (SELECT r.gid,d.datname AS database,r.type,r.backend,r.nodes,r.status
           FROM pg_catalog.rxact_get_running() AS r
@@ -38,7 +38,7 @@ CREATE OR REPLACE VIEW rxact_get_running AS
         LEFT JOIN pg_catalog.pgxc_node AS n
         ON r.nodes[r.i] = n.oid;
 
-CREATE VIEW pg_aux_tables AS
+CREATE VIEW pg_catalog.pg_aux_tables AS
     SELECT
         N.nspname AS schemaname,
         U.auxrelid::regclass AS tablename,
