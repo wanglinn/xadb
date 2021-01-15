2d964f7e4168565f96c67a1c08d91915abaeba40
ycy <yuecy@asiainfo.com>
2021-01-15 16:15:45 +0800

    修复 add datanode ,monitor all时告警信息不准确的问题

--- src/adbmgrd/manager/mgr_helper.c
+++ src/adbmgrd/manager/mgr_helper.c
@@ -4672,11 +4672,11 @@ warnning_master_has_no_sync(char 	*nodeName,
 												nodeType,
 												spiContext);
 			Assert(slaveNode);
-			if (is_equal_string(getMgrNodeSyncStateValue(SYNC_STATE_SYNC), NameStr(slaveNode->form.nodesync)))
-				return;
-			
-			mgrNode = selectMgrNodeByOid(slaveNode->form.nodemasternameoid, spiContext);
-			Assert(mgrNode);			
+			if (!is_equal_string(getMgrNodeSyncStateValue(SYNC_STATE_SYNC), NameStr(slaveNode->form.nodesync)))
+			{			
+				mgrNode = selectMgrNodeByOid(slaveNode->form.nodemasternameoid, spiContext);
+				Assert(mgrNode);
+			}			
 		}
 		else
 		{
@@ -4686,7 +4686,7 @@ warnning_master_has_no_sync(char 	*nodeName,
 			Assert(mgrNode);
 		}
 
-		if (isMasterNode(mgrNode->form.nodetype, true))
+		if (mgrNode != NULL && isMasterNode(mgrNode->form.nodetype, true))
 		{
 			selectMgrSlaveNodes(mgrNode->oid,
 								getMgrSlaveNodetype(mgrNode->form.nodetype),
@@ -4695,7 +4695,7 @@ warnning_master_has_no_sync(char 	*nodeName,
 			dlist_foreach(iter, &nodes)
 			{
 				tmpNode = dlist_container(MgrNodeWrapper, link, iter.cur);
-				if(is_equal_string(getMgrNodeSyncStateValue(SYNC_STATE_SYNC), NameStr(mgrNode->form.nodesync)))
+				if(is_equal_string(getMgrNodeSyncStateValue(SYNC_STATE_SYNC), NameStr(tmpNode->form.nodesync)))
 				{
 					hasSync = true;
 					break;
@@ -4704,8 +4704,7 @@ warnning_master_has_no_sync(char 	*nodeName,
 			if (!hasSync)
 				ereportWarningLog(errmsg("%s is %s, it has no synchronous node.",
 					NameStr(mgrNode->form.nodename),  mgr_get_nodetype_desc(mgrNode->form.nodetype)));
-		}
-		
+		}	
 	}PG_CATCH();
 	{
 		(void)MemoryContextSwitchTo(oldContext);
